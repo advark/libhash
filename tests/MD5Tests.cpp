53cdecd924b8279109eb7c839badf1100dca2f49
Yanick Poirier <ypoirier at hotmail dot com>
2019-11-14 17:15:34 -0500

    Code cleaning

--- tests/MD5Tests.cpp
+++ tests/MD5Tests.cpp
@@ -18,33 +18,14 @@
 using namespace libhash;
 
 void libHashTestCases::testMD5( ) {
-    char *msg;
     MD5 md5;
 
-    uint8_t result[md5.getHashSize( ) / 8];
+    for( int i = 0; i < sizeof ( testCases ) / sizeof ( TestData ); i++ ) {
+        runSingleChunk( i + 1, md5, "MD5", testCases[i].data, testCases[i].size, testCases[i].md5 );
+    }
 
     for( int i = 0; i < sizeof ( testCases ) / sizeof ( TestData ); i++ ) {
-        md5.init( );
-        md5.update( testCases[i].data, testCases[i].size );
-        md5.finalize( );
-        md5.getValue( result, sizeof ( result ) );
-
-        if( memcmp( result, testCases[i].md5, sizeof ( result ) ) != 0 ) {
-            asprintf( &msg,
-                      "MD5 Test #%d failed.\n"
-                      "     Result  = %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n"
-                      "     Expected= %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-                      i + 1,
-                      result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
-                      result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15],
-                      testCases[i].md5[0], testCases[i].md5[1], testCases[i].md5[2], testCases[i].md5[3],
-                      testCases[i].md5[4], testCases[i].md5[5], testCases[i].md5[6], testCases[i].md5[7],
-                      testCases[i].md5[8], testCases[i].md5[9], testCases[i].md5[10], testCases[i].md5[11],
-                      testCases[i].md5[12], testCases[i].md5[13], testCases[i].md5[14], testCases[i].md5[15] );
-
-            CPPUNIT_FAIL( msg );
-            free( msg );
-        }
+        runMultiChunk( 11 + i, md5, "MD5", testCases[i].data, testCases[i].size, ( testCases[i].size * 0.13 ) + 1, testCases[i].md5 );
     }
 }
 
